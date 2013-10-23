b086f7870554ca1b18f2fd3d243dfd69f8f4cb83
Gabriel Pedote <gabriel.pedote@fatec.sp.gov.br>
2013-10-23 18:18:25 -0200

    Cart Not Needed

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -63,18 +63,6 @@ class AppSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
 	);
 
-	public $carts = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'sessionid' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'book_id' => array('type' => 'integer', 'null' => false, 'default' => null),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
-
 	public $emails = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
 		'person_id' => array('type' => 'integer', 'null' => false, 'default' => null),
